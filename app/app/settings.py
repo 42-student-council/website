85f6f5d1c00c775d73e95f416db875439deeb975
Arthur Bied-Charreton <arthurbiedchar@gmail.com>
2024-06-07 15:34:01 +0200

    ADD: Nginx revers Proxy to avoid exposing the backend directly.

--- app/app/settings.py
+++ app/app/settings.py
@@ -39,6 +39,7 @@ INSTALLED_APPS = [
     "django.contrib.messages",
     "django.contrib.staticfiles",
     "db",
+    "corsheaders",
 ]
 
 MIDDLEWARE = [
@@ -49,6 +50,12 @@ MIDDLEWARE = [
     "django.contrib.auth.middleware.AuthenticationMiddleware",
     "django.contrib.messages.middleware.MessageMiddleware",
     "django.middleware.clickjacking.XFrameOptionsMiddleware",
+    "corsheaders.middleware.CorsMiddleware",
+]
+
+CORS_ALLOWED_ORIGINS = [
+    "http://localhost",
+    "http://localhost:3000",
 ]
 
 ROOT_URLCONF = "app.urls"
