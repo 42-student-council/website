ef65e139f1094adee03be7cedabcae9355f1de93
Arthur Bied-Charreton <arthurbiedchar@gmail.com>
2024-06-07 19:32:39 +0200

    ADD: Newline interpreting in inner HTML (about.jsx).

--- app/db/views.py
+++ app/db/views.py
@@ -10,6 +10,12 @@ def get_data(request):
         "About": {
             "paragraph": """
             We are students who have been elected by our peers to represent them in the school's decision making process.
+            This is a platform for you to anonymously share your thoughts, ideas, and concerns with us.
+            """,
+            "why": """
+            Because we want everyone to be heard.
+            Because we want transparent communication.
+            Because we want to make a difference.
             """,
         },
     }
--- app/front/src/components/about.jsx
+++ app/front/src/components/about.jsx
@@ -4,6 +4,9 @@ export const About = (props) => {
   const paragraph = props.data 
     ? props.data.paragraph.replace(/\n/g, '<br>') 
     : 'loading...';
+  const why = props.data
+    ? props.data.why.replace(/\n/g, '<br>')
+    : 'loading...';
 
   return (
     <div id="about">
@@ -17,6 +20,8 @@ export const About = (props) => {
             <div className="about-text">
               <h2>What is the Student Council?</h2>
               <p dangerouslySetInnerHTML={{ __html: paragraph }} />
+              <h3 style={{ marginBottom: '1px' }}>WHY?</h3>
+              <p style={{ marginTop: '1px' }} dangerouslySetInnerHTML={{ __html: why }} />
             </div>
           </div>
         </div>
