c5b6c59ad7d04fd583ae41fe06fb149623fb0f70
Arthur Bied-Charreton <arthurbiedchar@gmail.com>
2024-06-08 14:15:41 +0200

    ADD: CreateIssueView to urls.py.

--- app/app/urls.py
+++ app/app/urls.py
@@ -4,7 +4,7 @@ from db import views
 
 urlpatterns = [
     path("api/frontend/data/", views.get_frontend_data, name="get_frontend_data"),
-    path("api/issue/create/", views.create_issue, name="create_issue"),
+    path("api/issue/create/", views.CreateIssueView.as_view(), name="create_issue"),
     path("api/issues/view/all/", views.IssueListView.as_view(), name="view_issues"),
     path("api/issues/view/<int:issue_id>/", views.IssueView.as_view(), name="view_issue"),
 ]
