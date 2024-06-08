9a9e43d49beaa70dd51b88fd14462540e87cb7e7
Simon Dablander <ifaoji@pm.me>
2024-06-08 23:44:37 +0200

    style(front): run prettier

--- app/front/app/utils/oauth.server.ts
+++ app/front/app/utils/oauth.server.ts
@@ -1,83 +1,73 @@
-import { nanoid } from "nanoid";
+import { nanoid } from 'nanoid';
 
-const SCOPE = "public";
+const SCOPE = 'public';
 
 // TODO: check those env vars
 const oauthUrl = `https://api.intra.42.fr/oauth/authorize?client_id=${
-	process.env.CLIENT_ID
-}&redirect_uri=${encodeURIComponent(
-	`${process.env.BASE_URL}/oauth/callback`,
-)}&response_type=code&scope=${SCOPE}`;
+    process.env.CLIENT_ID
+}&redirect_uri=${encodeURIComponent(`${process.env.BASE_URL}/oauth/callback`)}&response_type=code&scope=${SCOPE}`;
 
 export function generateOauthUrl(state: string) {
-	return `${oauthUrl}&state=${state}`;
+    return `${oauthUrl}&state=${state}`;
 }
 
 // TODO: plz better state handling
-const stateStorage = new Map<
-	string,
-	{ redirectTo: string; timeout?: NodeJS.Timeout }
->();
+const stateStorage = new Map<string, { redirectTo: string; timeout?: NodeJS.Timeout }>();
 
 export function createState(redirectTo: string): string {
-	const state = nanoid();
-	const data = {
-		redirectTo,
-		timeoutId: undefined as undefined | NodeJS.Timeout,
-	};
-	stateStorage.set(state, data);
+    const state = nanoid();
+    const data = {
+        redirectTo,
+        timeoutId: undefined as undefined | NodeJS.Timeout,
+    };
+    stateStorage.set(state, data);
 
-	data.timeoutId = setTimeout(() => {
-		stateStorage.delete(state);
-	}, 600000); // 10 minutes
+    data.timeoutId = setTimeout(() => {
+        stateStorage.delete(state);
+    }, 600000); // 10 minutes
 
-	return state;
+    return state;
 }
 
 export function checkState(state: string): string | null {
-	const data = stateStorage.get(state);
-	if (!data) return null;
+    const data = stateStorage.get(state);
+    if (!data) return null;
 
-	if (data.timeout) clearTimeout(data.timeout);
+    if (data.timeout) clearTimeout(data.timeout);
 
-	stateStorage.delete(state);
+    stateStorage.delete(state);
 
-	return data.redirectTo;
+    return data.redirectTo;
 }
 
 function encodeFormData(data: object) {
-	return Object.keys(data)
-		.map(
-			(key) =>
-				`${encodeURIComponent(key)}=${encodeURIComponent(
-					data[key as keyof typeof data],
-				)}`,
-		)
-		.join("&");
+    return Object.keys(data)
+        .map((key) => `${encodeURIComponent(key)}=${encodeURIComponent(data[key as keyof typeof data])}`)
+        .join('&');
 }
 
 export async function getTokens(code: string): Promise<Tokens> {
-	const formData = encodeFormData({
-		grant_type: "authorization_code",
-		client_id: process.env.CLIENT_ID,
-		client_secret: process.env.CLIENT_SECRET,
-		code,
-		redirect_uri: `${process.env.BASE_URL}/oauth/callback`,
-	});
+    const formData = encodeFormData({
+        grant_type: 'authorization_code',
+        client_id: process.env.CLIENT_ID,
+        client_secret: process.env.CLIENT_SECRET,
+        code,
+        redirect_uri: `${process.env.BASE_URL}/oauth/callback`,
+    });
 
-	const response = await fetch("https://api.intra.42.fr/oauth/token", {
-		method: "POST",
-		headers: {
-			"Content-Type": "application/x-www-form-urlencoded",
-		},
-		body: formData,
-	});
+    const response = await fetch('https://api.intra.42.fr/oauth/token', {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/x-www-form-urlencoded',
+        },
+        body: formData,
+    });
 
-	if (!response.ok) {
-		throw new Error(`HTTP error! status: ${response.status}`);
-	}
+    if (!response.ok) {
+        throw new Error(`HTTP error! status: ${response.status}`);
+    }
 
-	return await response.json();
+    return await response.json();
 }
 
 // TODO: Do we even need to refresh tokens?
@@ -99,11 +89,11 @@ export async function getTokens(code: string): Promise<Tokens> {
 // }
 
 interface Tokens {
-	access_token: string;
-	refresh_token: string;
-	token_type: "bearer";
-	expires_in: number;
-	scope: string;
-	created_at: number;
-	secret_valid_until: number;
+    access_token: string;
+    refresh_token: string;
+    token_type: 'bearer';
+    expires_in: number;
+    scope: string;
+    created_at: number;
+    secret_valid_until: number;
 }
--- app/front/app/utils/session.server.ts
+++ app/front/app/utils/session.server.ts
@@ -1,141 +1,125 @@
-import {
-	type Session,
-	createCookieSessionStorage,
-	redirect,
-	createSessionStorage,
-} from "@remix-run/node";
-import { nanoid } from "nanoid";
+import { type Session, createCookieSessionStorage, redirect, createSessionStorage } from '@remix-run/node';
+import { nanoid } from 'nanoid';
 
 const sessionSecret = process.env.SESSION_SECRET;
 if (!sessionSecret) {
-	throw new Error("SESSION_SECRET must be set");
+    throw new Error('SESSION_SECRET must be set');
 }
 
 const storage = createCookieSessionStorage({
-	cookie: {
-		name: "session",
-		// normally you want this to be `secure: true`
-		// but that doesn't work on localhost for Safari
-		// https://web.dev/when-to-use-local-https/
-		secure: process.env.NODE_ENV === "production",
-		secrets: [sessionSecret],
-		sameSite: "lax",
-		path: "/",
-		maxAge: 60 * 60 * 24 * 30,
-		httpOnly: true,
-	},
+    cookie: {
+        name: 'session',
+        // normally you want this to be `secure: true`
+        // but that doesn't work on localhost for Safari
+        // https://web.dev/when-to-use-local-https/
+        secure: process.env.NODE_ENV === 'production',
+        secrets: [sessionSecret],
+        sameSite: 'lax',
+        path: '/',
+        maxAge: 60 * 60 * 24 * 30,
+        httpOnly: true,
+    },
 });
 
+export async function createSession(data: Omit<SessionData, 'id'>, redirectTo: string) {
+    const session = await storage.getSession();
 
-export async function createSession(
-	data: Omit<SessionData, "id">,
-	redirectTo: string,
-) {
-	const session = await storage.getSession();
+    session.set('accessToken', data.accessToken);
+    session.set('refreshToken', data.refreshToken);
+    session.set('accessTokenExpiresAt', data.accessTokenExpiresAt);
+    session.set('login', data.login);
+    session.set('createdAt', data.createdAt);
+    session.set('imageUrl', data.imageUrl);
 
-	session.set("accessToken", data.accessToken);
-	session.set("refreshToken", data.refreshToken);
-	session.set("accessTokenExpiresAt", data.accessTokenExpiresAt);
-	session.set("login", data.login);
-	session.set("createdAt", data.createdAt);
-	session.set("imageUrl", data.imageUrl);
-
-	return redirect(redirectTo, {
-		headers: {
-			"Set-Cookie": await storage.commitSession(session),
-		},
-	});
+    return redirect(redirectTo, {
+        headers: {
+            'Set-Cookie': await storage.commitSession(session),
+        },
+    });
 }
 
-export async function destroySession(
-	request: Request,
-	redirectTo = "/sign-in",
-): Promise<Response> {
-	const session = await getSession(request);
-	if (!session) return redirect(redirectTo);
+export async function destroySession(request: Request, redirectTo = '/sign-in'): Promise<Response> {
+    const session = await getSession(request);
+    if (!session) return redirect(redirectTo);
 
-	return redirect(redirectTo, {
-		headers: {
-			"Set-Cookie": await storage.destroySession(session),
-		},
-	});
+    return redirect(redirectTo, {
+        headers: {
+            'Set-Cookie': await storage.destroySession(session),
+        },
+    });
 }
 
-export async function getSession(
-	request: Request,
-): Promise<Session<SessionData> | null> {
-	const session = await storage.getSession(request.headers.get("Cookie"));
-	if (!isSessionData(session?.data)) return null;
+export async function getSession(request: Request): Promise<Session<SessionData> | null> {
+    const session = await storage.getSession(request.headers.get('Cookie'));
+    if (!isSessionData(session?.data)) return null;
 
-	return session;
+    return session;
 }
 
 export async function getUserLogin(request: Request): Promise<string | null> {
-	const session = await getSession(request);
-	const login = session?.get("login");
-	if (typeof login !== "string") {
-		return null;
-	}
+    const session = await getSession(request);
+    const login = session?.get('login');
+    if (typeof login !== 'string') {
+        return null;
+    }
 
-	return login;
+    return login;
 }
 
 export async function requireUserLogin(
-	request: Request,
-	redirectTo: string = new URL(request.url).pathname,
+    request: Request,
+    redirectTo: string = new URL(request.url).pathname,
 ): Promise<string> {
-	const session = await getSession(request);
-	const login = session?.get("login");
-	if (typeof login !== "string") {
-		const searchParams = new URLSearchParams([["redirectTo", redirectTo]]);
+    const session = await getSession(request);
+    const login = session?.get('login');
+    if (typeof login !== 'string') {
+        const searchParams = new URLSearchParams([['redirectTo', redirectTo]]);
 
-		throw redirect(`/sign-in?${searchParams}`);
-	}
+        throw redirect(`/sign-in?${searchParams}`);
+    }
 
-	return login;
+    return login;
 }
 
-export async function getSessionData(
-	request: Request,
-): Promise<SessionData | null> {
-	const session = await getSession(request);
-	const data = session?.data;
-	if (!isSessionData(data)) {
-		return null;
-	}
+export async function getSessionData(request: Request): Promise<SessionData | null> {
+    const session = await getSession(request);
+    const data = session?.data;
+    if (!isSessionData(data)) {
+        return null;
+    }
 
-	return data;
+    return data;
 }
 
 export async function requireSessionData(
-	request: Request,
-	redirectTo: string = new URL(request.url).pathname,
+    request: Request,
+    redirectTo: string = new URL(request.url).pathname,
 ): Promise<SessionData> {
-	const session = await getSession(request);
-	const data = session?.data;
-	if (!isSessionData(data)) {
-		const searchParams = new URLSearchParams([["redirectTo", redirectTo]]);
+    const session = await getSession(request);
+    const data = session?.data;
+    if (!isSessionData(data)) {
+        const searchParams = new URLSearchParams([['redirectTo', redirectTo]]);
 
-		throw redirect(`/sign-in?${searchParams}`);
-	}
+        throw redirect(`/sign-in?${searchParams}`);
+    }
 
-	return data;
+    return data;
 }
 
 // biome-ignore lint/suspicious/noExplicitAny: -
 function isSessionData(data: any): data is SessionData {
-	return (
-		// biome-ignore lint/complexity/useOptionalChain: -
-		data && data.imageUrl && data.login && data.createdAt && data.type
-	);
+    return (
+        // biome-ignore lint/complexity/useOptionalChain: -
+        data && data.imageUrl && data.login && data.createdAt && data.type
+    );
 }
 
 export interface SessionData {
-	imageUrl: string;
-	login: string;
-	id: string;
-	accessToken: string | null;
-	refreshToken: string | null;
-	accessTokenExpiresAt: Date | null;
-	createdAt: Date;
+    imageUrl: string;
+    login: string;
+    id: string;
+    accessToken: string | null;
+    refreshToken: string | null;
+    accessTokenExpiresAt: Date | null;
+    createdAt: Date;
 }
