5eda217db39f21dfb4dba602d260dcae4f40b3a9
Simon Dablander <ifaoji@pm.me>
2024-06-09 00:01:52 +0200

    chore(front): remove debug console.log

--- app/front/app/routes/_auth.oauth.callback.tsx
+++ app/front/app/routes/_auth.oauth.callback.tsx
@@ -27,8 +27,6 @@ export const loader: LoaderFunction = async ({ request }) => {
         const viennaCampus = apiUser?.campus_users?.find((campus: any) => campus.campus_id === 53);
         if (!viennaCampus?.is_primary) throw redirect('/sign-in?wrongCampus');
 
-        console.log(apiUser);
-
         return createSession(
             {
                 imageUrl: apiUser.image.versions.large,
