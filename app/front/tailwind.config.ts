9854e6ee161cc0fb4fc3d34321d41e9481c59025
Simon Dablander <ifaoji@pm.me>
2024-06-08 15:54:36 +0200

    refactor(front): fix default font for tailwind

--- app/front/tailwind.config.ts
+++ app/front/tailwind.config.ts
@@ -18,6 +18,9 @@ const config = {
       },
     },
     extend: {
+      fontFamily: {
+        sans: ["Noto Sans", "Open Sans", "sans-serif"],
+      },
       colors: {
         border: "hsl(var(--border))",
         input: "hsl(var(--input))",
