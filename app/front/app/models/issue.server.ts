f06aa2b1ba5656776d47ee39c314f84b7076acd1
Simon Dablander <ifaoji@pm.me>
2024-06-11 17:36:18 +0200

    style(front): run prettier

--- app/front/app/models/issue.server.ts
+++ app/front/app/models/issue.server.ts
@@ -1,13 +1,10 @@
-import { db } from "~/db.server";
+import { db } from '~/db.server';
 
-export async function createIssue(data: {
-	description: string;
-	title: string
-}) {
-	return await db.issue.create({
-		data: {
-			description: data.description,
-			title: data.title
-		}
-	});
+export async function createIssue(data: { description: string; title: string }) {
+    return await db.issue.create({
+        data: {
+            description: data.description,
+            title: data.title,
+        },
+    });
 }
