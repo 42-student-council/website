963361512cce99f32572afdd6319e3bd408d3f38
Simon Dablander <ifaoji@pm.me>
2024-06-19 00:11:36 +0200

    chore: delete unwanted files - for this pr

--- app/front/app/components/ui/Blockquote.tsx
+++
@@ -1,6 +0,0 @@
-import type { ReactNode } from 'react';
-import classNames from 'classnames';
-
-export function Blockquote({ children, className }: { children: ReactNode; className?: string }) {
-    return <blockquote className={classNames('mt-6 border-l-2 pl-6 italic', className)}>{children}</blockquote>;
-}
--- app/front/app/components/ui/H4.tsx
+++
@@ -1,6 +0,0 @@
-import type { ReactNode } from 'react';
-import classNames from 'classnames';
-
-export function H4({ children, className }: { children: ReactNode; className?: string }) {
-    return <h4 className={classNames('scroll-m-20 text-xl font-semibold tracking-tight', className)}>{children}</h4>;
-}
