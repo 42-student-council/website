a2e95e593353b7dd06447edbc11592a8c1d5cd00
Simon Dablander <ifaoji@pm.me>
2024-07-08 19:59:27 +0200

    feat(front): add db util

---
+++ app/front/app/utils/db.server.ts
@@ -0,0 +1,18 @@
+import { PrismaClient } from '@prisma/client';
+
+let db: PrismaClient;
+
+declare global {
+    var __db: PrismaClient | undefined;
+}
+
+if (process.env.NODE_ENV === 'production') {
+    db = new PrismaClient();
+} else {
+    if (!global.__db) {
+        global.__db = new PrismaClient();
+    }
+    db = global.__db;
+}
+
+export { db };
