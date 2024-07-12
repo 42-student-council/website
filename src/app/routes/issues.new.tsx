af8bf390b74a2be0f670c45ca664bc19b6661953
Simon Dablander <ifaoji@pm.me>
2024-07-12 19:54:55 +0200

    fix: create issue logout button

--- src/app/routes/issues.new.tsx
+++ src/app/routes/issues.new.tsx
@@ -110,7 +110,7 @@ export default function IssuesNew() {
 
     return (
         <div>
-            <NavBar login={data.login} role={data.role} />
+            <NavBar login={data.session.login} role={data.session.role} />
             <div className='md:flex md:justify-center'>
                 <H1 className='m-4 md:w-3/5'>Create a Public Issue</H1>
             </div>
