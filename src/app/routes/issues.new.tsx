01930ce0bbd6694596b81ea31a39991baebda84e
Lukas <129603980+itislu@users.noreply.github.com>
2024-09-12 16:54:30 +0200

    revert: add back issue ID to title on student server (#173)
    * revert: add back issue ID to title on student server
    
    I know we agreed that it's not necessary to have the ID in the title.
    But I just figured that we haven't considered that that opens up possibilities for people to troll.
    They can just create an issue with the same title as a hot issue but write some spam inside it.
    
    * fix: fix capitalization of "issue"

--- src/app/routes/issues.new.tsx
+++ src/app/routes/issues.new.tsx
@@ -104,8 +104,8 @@ export async function action({ request }: ActionFunctionArgs) {
                         .catch(console.error);
 
                     await sendDiscordWebhookWithUrl(config.discord.studentServerIssueWebhookUrl, {
-                        thread_name: `${data.title}`,
-                        content: `[A new Issue has been opened on the Student Council Website.](${config.baseUrl}/issues/${issue.id})`,
+                        thread_name: `${data.title} - #${issue.id}`,
+                        content: `[A new issue has been opened on the Student Council Website.](${config.baseUrl}/issues/${issue.id})`,
                         wait: true,
                     })
                         .then(
