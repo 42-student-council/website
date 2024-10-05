ba776844ad57a1cacdff2b8d20e2a7710143e188
leeoocca <leeoocca@gmail.com>
2024-10-05 23:39:48 +0200

    refactor: extract MarkdownBadge component

--- src/app/routes/issues.$id.tsx
+++ src/app/routes/issues.$id.tsx
@@ -6,7 +6,7 @@ import { RateLimiterMemory } from 'rate-limiter-flexible';
 import { FormEvent, useEffect, useRef, useState } from 'react';
 import { z } from 'zod';
 import { FormErrorMessage } from '~/components/FormErrorMessage';
-import Markdown from '~/components/Markdown';
+import Markdown, { MarkdownBadge } from '~/components/Markdown';
 import NavBar from '~/components/NavBar';
 import { Info } from '~/components/alert/Info';
 import { H1 } from '~/components/ui/H1';
@@ -532,18 +532,7 @@ export default function IssueDetail() {
                                             </label>
                                         </div>
                                     )}
-                                    <div className='p-2 text-sm text-muted-foreground font-medium leading-none flex items-center space-x-2'>
-                                        <svg
-                                            xmlns='http://www.w3.org/2000/svg'
-                                            viewBox='0 0 16 16'
-                                            width='24'
-                                            height='24'
-                                            fill='currentColor'
-                                        >
-                                            <path d='M14.85 3c.63 0 1.15.52 1.14 1.15v7.7c0 .63-.51 1.15-1.15 1.15H1.15C.52 13 0 12.48 0 11.84V4.15C0 3.52.52 3 1.15 3ZM9 11V5H7L5.5 7 4 5H2v6h2V8l1.5 1.92L7 8v3Zm2.99.5L14.5 8H13V5h-2v3H9.5Z'></path>
-                                        </svg>
-                                        <span className='hidden sm:block'>Markdown is supported</span>
-                                    </div>
+                                    <MarkdownBadge />
                                     <Button type='submit' invalid={!isFormValid}>
                                         Comment
                                     </Button>
