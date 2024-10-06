c9b0b3fbb39b1425a2e1c6901af3608688dd5b77
leeoocca <36135198+leeoocca@users.noreply.github.com>
2024-10-06 18:08:13 +0200

    fix: remove extra footer on error pages (#191)

--- src/app/root.tsx
+++ src/app/root.tsx
@@ -103,7 +103,6 @@ export function ErrorBoundary() {
                         'Unknown Error'
                     )}
                 </div>
-                <Footer />
                 <Scripts />
             </body>
         </html>
