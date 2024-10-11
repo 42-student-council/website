e5bd2bd78bab2eecb03b4b26f44c1524b5931fb7
Simon <ifaoji@pm.me>
2024-10-12 00:39:22 +0200

    Apply suggestions from code review

--- src/app/utils/date.ts
+++ src/app/utils/date.ts
@@ -1,4 +1,4 @@
-const timezone = 'Europe/Vienna';
+const timeZone = 'Europe/Vienna';
 
 export function formatDate(date: Date) {
     const now = new Date();
@@ -8,7 +8,7 @@ export function formatDate(date: Date) {
     const isToday = date.toDateString() === now.toDateString();
     const isYesterday = date.toDateString() === yesterday.toDateString();
 
-    const timeString = date.toLocaleTimeString('de-DE', { hour: '2-digit', minute: '2-digit', timeZone: timezone });
+    const timeString = date.toLocaleTimeString('de-DE', { hour: '2-digit', minute: '2-digit', timeZone });
 
     if (isToday) {
         return `Today at ${timeString}`;
@@ -18,6 +18,6 @@ export function formatDate(date: Date) {
         return `Yesterday at ${timeString}`;
     }
 
-    const dateString = date.toLocaleDateString('de-DE', { timeZone: timezone });
+    const dateString = date.toLocaleDateString('de-DE', { timeZone });
     return `${dateString} ${timeString}`;
 }
