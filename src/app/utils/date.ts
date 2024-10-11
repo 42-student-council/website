07928d52044a5c781fe38bf3b16219a019d5fd78
leeoocca <leeoocca@gmail.com>
2024-10-12 00:34:58 +0200

    fix: dates UTC flash

--- src/app/utils/date.ts
+++ src/app/utils/date.ts
@@ -1,3 +1,5 @@
+const timezone = 'Europe/Vienna';
+
 export function formatDate(date: Date) {
     const now = new Date();
     const yesterday = new Date();
@@ -6,7 +8,7 @@ export function formatDate(date: Date) {
     const isToday = date.toDateString() === now.toDateString();
     const isYesterday = date.toDateString() === yesterday.toDateString();
 
-    const timeString = date.toLocaleTimeString('de-DE', { hour: '2-digit', minute: '2-digit' });
+    const timeString = date.toLocaleTimeString('de-DE', { hour: '2-digit', minute: '2-digit', timeZone: timezone });
 
     if (isToday) {
         return `Today at ${timeString}`;
@@ -16,6 +18,6 @@ export function formatDate(date: Date) {
         return `Yesterday at ${timeString}`;
     }
 
-    const dateString = date.toLocaleDateString('de-DE');
+    const dateString = date.toLocaleDateString('de-DE', { timeZone: timezone });
     return `${dateString} ${timeString}`;
 }
